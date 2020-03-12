51f7a2adbf00b2ec171998bb9ce3f5fa45c72559
nitrocode <alexidowu25@gmail.com>
2020-03-12 17:53:42 +0100

    Finalized menu structure alongside look and feel to mobile nav

--- src/views/faq.vue
+++ src/views/faq.vue
@@ -202,7 +202,17 @@
         },
         methods: {},
         mounted() {
-            // console.log(this.getUserdata());
+            setTimeout(function() {
+                // Get the head tag
+                var head_ID = document.getElementsByTagName("head")[0];
+                // Create script element
+                var script_element = document.createElement('script');
+                // Set the script type to JavaScript
+                script_element.type = 'text/javascript';
+                // External JS file
+                script_element.src = 'js/custom.js';
+                head_ID.appendChild(script_element);
+            }, 300);
         }
     }
 </script>
