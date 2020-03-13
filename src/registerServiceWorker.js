0e7f3be49beefee982ca0d2af54d29dc8c1b88c6
nitrocode <alexidowu25@gmail.com>
2020-03-13 11:04:35 +0100

    Finalized menu structure alongside look and feel to mobile nav

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,8 +2,8 @@
 
 import { register } from 'register-service-worker'
 
-if (process.env.NODE_ENV === 'production') {
-// if (process.env.NODE_ENV) {
+// if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV) {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
