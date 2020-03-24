9e29d671d109595c8b6221deb73394b1bb044564
nitrocode <alexidowu25@gmail.com>
2020-03-24 01:03:55 +0100

    fixed account board navigation issue

--- src/views/Auth/account-board.vue
+++ src/views/Auth/account-board.vue
@@ -171,6 +171,19 @@
                 store.dispatch('authInit', authMetadata)
                     .then();
             }
+        },
+        mounted() {
+            setTimeout(function() {
+                // Get the head tag
+                var head_ID = document.getElementsByTagName("head")[0];
+                // Create script element
+                var script_element = document.createElement('script');
+                // Set the script type to JavaScript
+                script_element.type = 'text/javascript';
+                // External JS file
+                script_element.src = 'js/custom.js';
+                head_ID.appendChild(script_element);
+            }, 200);
         }
     }
 </script>
